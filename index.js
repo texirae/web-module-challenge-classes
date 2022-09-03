255be843222e00f39b38cbc8467d118716238dff
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-09-03 11:10:01 -0500

    completed task 4

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ class Car {
 */
 
 class Lambdasian {
-  constructor(name, age, location){
+  constructor({name, age, location}){
     this.name = name;
     this.age = age;
     this.location = location;
@@ -138,9 +138,20 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
-}
+class Instructor extends Lambdasian{
+  constructor({name, age, location, specialty, favLanguage, catchPhrase}){
+    super({name, age, location, specialty, favLanguage, catchPhrase});
+    this.specialty = specialty;
+    this.favLanguage = favLanguage;
+    this.catchPhrase = catchPhrase;
+    }
+    demo(subject){
+      return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject){
+      return `${student.name} recieves a perfect score on ${subject}`;
+    }
+  }
 
 /*
   TASK 5
