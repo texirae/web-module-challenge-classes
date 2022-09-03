b11692de919fc3d2daf4fb6a4c8db3b058c9ebc2
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-09-03 11:31:23 -0500

    completed mvp

--- index.js
+++ index.js
@@ -201,8 +201,18 @@ class Student extends Lambdasian{
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor{
+   constructor({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor}){
+    super({name, age, location, specialty, favLanguage, catchPhrase, gradClassName, favInstructor});
+    this.gradClassName = gradClassName;
+    this.favInstructor = favInstructor;
+   }
+   standUp(slackChannel){
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+   }
+   debugsCode(student, subject){
+    return `${this.name} debugs ${student.name}'s code on ${subject}`;
+   }
 }
 
 /*
